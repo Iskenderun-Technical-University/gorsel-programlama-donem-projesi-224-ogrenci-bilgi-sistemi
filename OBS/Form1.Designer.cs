077b42c28b810fbaba79c1d67c380f2199ee7c6d
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2022-10-17 23:19:56 +0300

    mfg asd

--- OBS/Form1.Designer.cs
+++ OBS/Form1.Designer.cs
@@ -28,32 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(230, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "mfg";
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(176, 139);
+            this.button1.Location = new System.Drawing.Point(421, 329);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 43);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 68);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "bb";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 297);
+            this.ClientSize = new System.Drawing.Size(541, 396);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private Label label1;
         private Button button1;
     }
 }
