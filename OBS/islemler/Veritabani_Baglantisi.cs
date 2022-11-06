760c5f6dc68ab13e8d69845437e8ee19dc538a32
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2022-11-06 17:35:44 +0300

    Veri tabanı bağlantısı ve giriş kontrolü için ayrı ayrı class'lar oluşturuldu

---
+++ OBS/islemler/Veritabani_Baglantisi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace OBS.islemler
+{
+    // veri tabanı bağlantısı gerçekleştirme 
+    class Veritabani_Baglantisi
+    {
+        static SqlConnection con;
+        static SqlCommand cmd;
+        static SqlDataAdapter da;
+        static DataSet ds;
+
+        public static string sqlCon = @"Data Source=DESKTOP-K2C3H0A;Initial Catalog = Universite_Obs; Integrated Security = True";
+
+        // bağlantı kontrolü
+        public static bool baglantiKontrol()
+        {
+            using(con=new SqlConnection(sqlCon))
+            {
+                try
+                {
+                    con.Open();
+                    return true;
+                }
+                catch(SqlException exp)
+                {
+                    MessageBox.Show(exp.Message);
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ OBS/islemler/girisKontrol.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using OBS.islemler;
+
+namespace OBS.islemler
+{
+    internal class girisKontrol
+    {
+        static SqlConnection con;
+        static SqlCommand cmd;
+        static SqlDataReader dr;
+        static DataSet ds;
+
+        public static string sqlcon = @"Data Source=DESKTOP-K2C3H0A;Initial Catalog = Universite_Obs; Integrated Security = True";
+
+        // akademisyen Girisi
+        
+        public static bool akademisyenGirisi(string kullaniciAdi,string sifre)
+        {
+            string sorgu = "select * from tbl_akademisyen where akdid=@user and akdSifre=@pass";
+
+            con = new SqlConnection(sqlcon);
+            cmd = new SqlCommand(sorgu, con);
+            cmd.Parameters.AddWithValue("@user", kullaniciAdi);
+            cmd.Parameters.AddWithValue("@pass", sifre);
+
+            con.Open();
+            dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                con.Close();
+                return true;
+            }
+            else
+            {
+                con.Close();
+                return false;
+            }
+        }
+    }
+}
