e50a2a7041f2573b0bbc970a9104fa52770c3d2a
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2022-12-22 03:43:26 +0300

    akademisyen kişisel bilgiler ekranına şifre güncelleme formu eklendi

---
+++ OBS/akademisyen/akademisyenSifre.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace OBS.akademisyen
+{
+    partial class akademisyenSifre
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(akademisyenSifre));
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxCaptcha = new System.Windows.Forms.TextBox();
+            this.labelCaptcha = new System.Windows.Forms.Label();
+            this.textBoxEskiSifre = new System.Windows.Forms.TextBox();
+            this.textBoxYeniSifre = new System.Windows.Forms.TextBox();
+            this.textBoxTekrar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(16, 600);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 24);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "bilgi";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 529);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 54);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Şifre Değiştir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxCaptcha
+            // 
+            this.textBoxCaptcha.Location = new System.Drawing.Point(321, 450);
+            this.textBoxCaptcha.Name = "textBoxCaptcha";
+            this.textBoxCaptcha.Size = new System.Drawing.Size(138, 27);
+            this.textBoxCaptcha.TabIndex = 18;
+            // 
+            // labelCaptcha
+            // 
+            this.labelCaptcha.AutoSize = true;
+            this.labelCaptcha.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelCaptcha.Location = new System.Drawing.Point(224, 450);
+            this.labelCaptcha.Name = "labelCaptcha";
+            this.labelCaptcha.Size = new System.Drawing.Size(91, 24);
+            this.labelCaptcha.TabIndex = 24;
+            this.labelCaptcha.Text = "captcha";
+            // 
+            // textBoxEskiSifre
+            // 
+            this.textBoxEskiSifre.Location = new System.Drawing.Point(224, 177);
+            this.textBoxEskiSifre.Name = "textBoxEskiSifre";
+            this.textBoxEskiSifre.PasswordChar = '*';
+            this.textBoxEskiSifre.Size = new System.Drawing.Size(235, 27);
+            this.textBoxEskiSifre.TabIndex = 15;
+            // 
+            // textBoxYeniSifre
+            // 
+            this.textBoxYeniSifre.Location = new System.Drawing.Point(224, 278);
+            this.textBoxYeniSifre.Name = "textBoxYeniSifre";
+            this.textBoxYeniSifre.PasswordChar = '*';
+            this.textBoxYeniSifre.Size = new System.Drawing.Size(235, 27);
+            this.textBoxYeniSifre.TabIndex = 16;
+            // 
+            // textBoxTekrar
+            // 
+            this.textBoxTekrar.Location = new System.Drawing.Point(224, 385);
+            this.textBoxTekrar.Name = "textBoxTekrar";
+            this.textBoxTekrar.PasswordChar = '*';
+            this.textBoxTekrar.Size = new System.Drawing.Size(235, 27);
+            this.textBoxTekrar.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(13, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 24);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Yeni Şifre (tekrar):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(13, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 24);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Yeni Şifre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(16, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 24);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Eski Şifre:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(183, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // akademisyenSifre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(472, 655);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxCaptcha);
+            this.Controls.Add(this.labelCaptcha);
+            this.Controls.Add(this.textBoxEskiSifre);
+            this.Controls.Add(this.textBoxYeniSifre);
+            this.Controls.Add(this.textBoxTekrar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "akademisyenSifre";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Şifre Değiştirme Ekranı";
+            this.Load += new System.EventHandler(this.akademisyenSifre_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label4;
+        private Button button1;
+        private TextBox textBoxCaptcha;
+        private Label labelCaptcha;
+        private TextBox textBoxEskiSifre;
+        private TextBox textBoxYeniSifre;
+        private TextBox textBoxTekrar;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox1;
+    }
+}
